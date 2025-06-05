f2723ec92ac48fe0fa980f66254ab14b18b73bc6
NguyenHaiQuoc <nguyenhaiquoc282@gmail.com>
2025-06-05 09:05:47 +0700

    Modify ip udfs

--- spark/main/udfs/ip_udfs.py
+++ spark/main/udfs/ip_udfs.py
@@ -1,43 +1,57 @@
 import IP2Location
 from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType
+from pyspark import SparkFiles
+
+ip2loc_instance = None
 
 @udf(StringType())
 def get_country(ip: str) -> str:
+    global ip2loc_instance
     if not ip or ip in ("", "unknown", "0.0.0.0"):
         return "Unknown"
     try:
-        db = IP2Location.IP2Location("main/resource/IP-COUNTRY-REGION-CITY.BIN")
-        record = db.get_all(ip)
+        if ip2loc_instance is None:
+            ip2loc_path = SparkFiles.get("IP-COUNTRY-REGION-CITY.BIN")
+            ip2loc_instance = IP2Location.IP2Location(ip2loc_path)
+
+        record = ip2loc_instance.get_all(ip)
         return record.country_long or "Unknown"
     except FileNotFoundError as e:
         raise FileNotFoundError(
-            "IP2Location database file not found. Please ensure the file exists at 'main/resource/IP-COUNTRY-REGION-CITY.BIN'."
+            "IP2Location database file not found."
         ) from e
-    
+
+
 @udf(StringType())
 def get_region(ip: str) -> str:
+    global ip2loc_instance
+
     if not ip or ip in ("", "unknown", "0.0.0.0"):
         return "Unknown"
     try:
-        db = IP2Location.IP2Location("main/resource/IP-COUNTRY-REGION-CITY.BIN")
-        record = db.get_all(ip)
+        if ip2loc_instance is None:
+            ip2loc_path = SparkFiles.get("IP-COUNTRY-REGION-CITY.BIN")
+            ip2loc_instance = IP2Location.IP2Location(ip2loc_path)
+
+        record = ip2loc_instance.get_all(ip)
         return record.region or "Unknown"
     except FileNotFoundError as e:
-        raise FileNotFoundError(
-            "IP2Location database file not found. Please ensure the file exists at 'main/resource/IP-COUNTRY-REGION-CITY.BIN'."
-        ) from e
+        raise FileNotFoundError("IP2Location database file not found.") from e
 
 @udf(StringType())
 def get_city(ip: str) -> str:
+    global ip2loc_instance
     if not ip or ip in ("", "unknown", "0.0.0.0"):
         return "Unknown"
     try:
-        db = IP2Location.IP2Location("main/resource/IP-COUNTRY-REGION-CITY.BIN")
-        record = db.get_all(ip)
+        if ip2loc_instance is None:
+            ip2loc_path = SparkFiles.get("IP-COUNTRY-REGION-CITY.BIN")
+            ip2loc_instance = IP2Location.IP2Location(ip2loc_path)
+
+        record = ip2loc_instance.get_all(ip)
         return record.city or "Unknown"
     except FileNotFoundError as e:
         raise FileNotFoundError(
-            "IP2Location database file not found. Please ensure the file exists at 'main/resource/IP-COUNTRY-REGION-CITY.BIN'."
+            "IP2Location database file not found."
         ) from e
-
